bf033c8910fe41129b15112888936612f70501ae
Archana Padala <Archana Padala@ACHU>
2024-07-30 19:56:43 -0500

    Add project files.

---
+++ Core1/Controllers/BankController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Core1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BankController : ControllerBase
+    {
+
+        [HttpGet]
+        [Route("Consuming/GetData")]
+        public IActionResult GetData()
+        {
+
+            try
+            {
+                HttpClient client = new HttpClient();
+                client.BaseAddress = new Uri("http://localhost:5001/api/Regions");
+                
+                HttpResponseMessage response = client.GetAsync("api/GetAll").Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    var data = response.Content.ReadAsStringAsync().Result;
+                    return Ok(data);
+
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+            catch (Exception ex)
+            {
+                return Ok(ex.Message);
+            }
+
+
+        }
+    }
+}
